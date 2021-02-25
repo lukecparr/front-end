6be9cb0d32ac70233eee4ebf9841dcfaca54015f
Lukecp <lukecp@pm.me>
2021-02-24 22:54:08 -0500

    Created custom useForm hook and implemented in UserLogin. Also added .gitignore file

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,28 +1,26 @@
 import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { useForm } from '../hooks';
 
 const UserLogin = () => {
-	const credentials = {
-		username: '',
-		password: ''
-	}
+	const initialState = { username: '', password: '' };
 	
-	const [formValues, setFormValues] = useState();
+	const [values, handleChanges, handleSubmit] = useForm(initialState, () => {console.log(values)});
 
 
 	return (
 		<>
 			<h1>Here's the login component</h1>
-			<Form /*onSubmit={handleSubmit}*/ >
+			<Form onSubmit={handleSubmit} >
 				<FormGroup>
 					<Label for='username'>Username</Label>
-					<Input type='text' name='username' /*value={formValues.username}*/ /*onChange={handleChanges}*/ />
+					<Input type='text' name='username' value={values.username} onChange={handleChanges} />
 				</FormGroup>
 				<FormGroup>
 					<Label for='password'>Password</Label>
-					<Input type='password' name='password' /*value={formValues.password}*/ /*onChange={handleChanges}*/ />
+					<Input type='password' name='password' value={values.password} onChange={handleChanges} />
 				</FormGroup>
-				<Button>Submit</Button>
+				<Button color="success">Submit</Button>
 			</Form>
 		</>
 	)
