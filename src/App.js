d2a5ba382ca363d6292cf37a24c1608abfc2328f
Lukecp <lukecp@pm.me>
2021-02-26 00:03:35 -0500

    completed NewUser form. Also added axios posts to NewUser and UserLogin using dummy backend

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import UserLogin from './components/UserLogin';
+import NewUser from './components/NewUser';
 
 function App() {
   return (
     <div className="App">
       <Route path='/login' component={UserLogin} />
+      <Route path='/sign-up' component={NewUser} />
     </div>
   );
 }
