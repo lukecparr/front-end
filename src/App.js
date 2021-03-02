5836c85558f8b2f27d3d294cb715edf284fb04df
Lukecp <lukecp@pm.me>
2021-03-01 23:14:05 -0500

    Set up contexts, Provider in App.js, and the consumer in EachClass

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import React, { useState } from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+
 import EachClass from "./components/EachClass"
-
-import dummydata from "./dummydata"
 import ClassList from "./components/ClassList"
-
 import UserLogin from './components/UserLogin';
 import NewUser from './components/NewUser';
 
-function App() {
-  const [classesState, setClassesState] = useState(dummydata);
-  console.log(classesState);
+import { classContext, userContext } from './contexts';
+import axiosWithAuth from './utils/axiosWithAuth';
 
-  //need to add links with NAV 
-  // need to add title
-  // need to switch statement
+import dummydata from './dummydata';
+
+function App() {
+  const [classes, setClasses] = useState(dummydata);
+
+  const fetchClasses = () => {
+		axiosWithAuth.get('/users/classes')
+			.then(res => setClasses(res.data))
+			.catch(err => console.log(err.message));
+	};
 
   return (
-    <div className="App">
-      <nav>
-        <h1 className="app-header">AnywhereFitness</h1>
-        <div className="nav-links">
-          <Link to="/class-list">Home - All Classes</Link>
-          <Link to="/login">Login</Link>
-        </div>
-      </nav>
-      <Switch>
-      <Route path="/class-list/:classID">
-        <EachClass classesD={classesState} />
-      </Route>
-      <Route path="/class-list">
-        <ClassList classesD={classesState} />
-      </Route>
-      <Route path='/login' component={UserLogin} />
-      <Route path='/sign-up' component={NewUser} />
-      </Switch>
-    </div>
-  );
+		<div className='App'>
+			<nav>
+				<h1 className='app-header'>AnywhereFitness</h1>
+				<div className='nav-links'>
+					<Link to='/class-list'>Home - All Classes</Link>
+					<Link to='/login'>Login</Link>
+				</div>
+			</nav>
+
+			<classContext.Provider value={classes}>
+				<Route path='/class-list/:classID' component={EachClass} />
+			</classContext.Provider>
+
+			<Route exact path='/class-list'>
+				<ClassList classesD={classes} />
+			</Route>
+
+			<Route path='/login' component={UserLogin} />
+			<Route path='/sign-up' component={NewUser} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/EachClass.js
+++ src/components/EachClass.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
+
+import { classContext } from '../contexts';
 
 // this component will be display each individual class once clicked on from the home page 
 // this will have all the details on each class and will also include the "Sign Up" button for users
 // this will NOT have any additional functionality for instructors
 function EachClass(props) {
-
     // useParams for rendering specific classes on the basis of their ID
     const params = useParams();
-
     // useHistory here to allow you to go back and forth between class selection and form submission
     const history = useHistory();
-
     // attached onclick event back to selection button
     const routeToHome = event => {
         history.push("/class-list")
     }
-
     //attached onclick event to sign-up form 
     const routeToSignUp = event => {
         history.push("/sign-up")
     }
-
     // iterating over arr of objects to find classes with matching ids as the one in the URL (params.itemID)
     // classSelected will be the variable that grabs each specific selected item. This will used in the JSX to display each class seperately 
-    const classSelected = props.classesD.find(eachClass => eachClass.id === Number(params.classID))
+    const classesD = useContext(classContext);
+    
+    const classSelected = classesD.find(eachClass => eachClass.id === Number(params.classID))
+
     return (
         <div className="class-wrapper">
             <button className="go-back-button" onClick={routeToHome}>Back to Class Selection..</button>
